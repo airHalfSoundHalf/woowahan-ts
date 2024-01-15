453039c30a419214535a2d270ba083a2de756878
tylee <bpthess@naver.com>
2024-01-16 00:44:29 +0900

    enum, as const, const enum by https://joshua1988.github.io/ts/guide/operator.html#union-type%EC%9D%98-%EC%9E%A5%EC%A0%90

---
+++ ts/src/TypeStudy.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+enum WeekDays {
+  MON = 'MON',
+  TUES = 'TUES',
+  WEDNES = 'WEDNES',
+  TYHURS = 'TYHURS',
+  FRI = 'FRI',
+}
+
+const MyComponent = () => {
+  type WeekDayKey = keyof typeof WeekDays;
+
+  function printDay(key: WeekDayKey, message: string) {
+    const day = WeekDays[key];
+
+    if (day <= WeekDays.WEDNES) {
+      console.log(`day:${day}, ${message}`);
+    }
+  }
+
+  printDay('TUES', 'test');
+
+  const booleanType = {
+    false: 0,
+    true: 1,
+  } as const;
+
+  type X = (typeof booleanType)[keyof typeof booleanType];
+
+  return <div></div>;
+};
+
+export default MyComponent;
