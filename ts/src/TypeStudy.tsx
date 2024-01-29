54f36b0b8d2790d68301941a79f7fa0e9a313f9d
tylee <bpthess@naver.com>
2024-01-30 02:11:23 +0900

    제네릭 공부

--- ts/src/TypeStudy.tsx
+++ ts/src/TypeStudy.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 
 enum WeekDays {
   MON = 'MON',
@@ -31,6 +31,59 @@ const MyComponent = () => {
   let noticePopup: Record<string, never>;
 
   console.log('noticePopup:', noticePopup);
+
+  type Example = {
+    a: number;
+    b: string;
+    c: boolean;
+  };
+
+  type Subset<T> = {
+    [K in typeof T]?: T[K];
+  };
+
+  const aExample: Subset<Example> = { a: 3 };
+
+  type Xx = {
+    readonly a: number;
+    readonly b: string;
+  };
+
+  type CreateMutable<Type> = {
+    readonly [Property in keyof Type]: Type[Property];
+  };
+
+  type ResultType = CreateMutable<Xx>;
+
+  const BottomSheetMap = {};
+
+  type BOTTOM_SHEET_ID = keyof typeof BottomSheetMap;
+
+  type BottomSheetStore = {
+    [index in BOTTOM_SHEET_ID as `${index}_BOTTOM_SHEET`]: {
+      resolver: () => void;
+      args: any;
+      isOpened: boolean;
+    };
+  };
+
+  type ExampleArrayType<T> = T[];
+
+  const x: ExampleArrayType<string> = ['1', '2'];
+
+  function exampleFunc<T>(arg: T): T[] {
+    return new Array(3).fill(arg);
+  }
+
+  interface SubmitEvent<T = HTMLElement> extends SyntheticEvent<T> {
+    submitter: T;
+  }
+
+  exampleFunc('hello');
+
+  const arrowExampeFunc = <T extends boolean>(arg: T): T[] => {
+    return new Array(3).fill(arg);
+  };
 
   return <div></div>;
 };
