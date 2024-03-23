8359b476ebbe4f3d7ad01e7ccc9656cde437cad1
tylee <bpthess@naver.com>
2024-03-23 23:37:13 +0900

    클래스 제네릭

--- ts/src/TypeStudy.tsx
+++ ts/src/TypeStudy.tsx
@@ -88,4 +88,42 @@ const MyComponent = () => {
   return <div></div>;
 };
 
-export default MyComponent;
+class LocalDB<T> {
+  async put(table: string, row: T): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      // T 타입의 데이터를 DB에 저장
+    });
+  }
+
+  async get(table: string, key: any): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      // T 타입의 데이터를 DB에서 가져옴
+    });
+  }
+
+  async getTaable(table: string): Promise<T[]> {
+    return new Promise<T[]>((resolve, reject) => {
+      // T[] 타입의 데이터를 DB에서 가져옴
+    });
+  }
+}
+
+class ICacheStore {}
+
+export default class IndexedDB implements ICacheStore {
+  private _DB: LocalDB<{ key: string; value: Promise<Record<string, unknown>>; cacheTTL: number }>;
+
+  private DB() {
+    if (!this._DB) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      this._DB = new LocalDB('localCache', {
+        ver: 6,
+        tables: [{ name: '', keyPath: 'key' }],
+      });
+      return this._DB;
+    }
+  }
+}
+
+// export default MyComponent;
